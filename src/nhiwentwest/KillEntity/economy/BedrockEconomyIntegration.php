34f3c056822cd791ba5462001f4505705fb670cd
nhiwentwest <156091743+nhiwentwest@users.noreply.github.com>
2024-01-10 21:47:03 +0700

    Add files via upload

---
+++ src/nhiwentwest/KillEntity/economy/BedrockEconomyIntegration.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace nhiwentwest\KillEntity\economy;
+
+use Closure;
+use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
+use cooldogedev\BedrockEconomy\BedrockEconomy;
+use cooldogedev\BedrockEconomy\libs\cooldogedev\libSQL\context\ClosureContext;
+use InvalidArgumentException;
+use pocketmine\player\Player;
+use nhiwentwest\KillEntity\Main;
+use pocketmine\Server;
+use function assert;
+
+final class BedrockEconomyIntegration implements EconomyIntegration{
+
+	private BedrockEconomy $plugin;
+
+	public function __construct(){
+		/** @var BedrockEconomy|null $plugin */
+		$plugin = Server::getInstance()->getPluginManager()->getPlugin("BedrockEconomy");
+		if($plugin === null){
+			throw new InvalidArgumentException("BedrockEconomy plugin was not found");
+		}
+
+		$this->plugin = $plugin;
+	}
+
+    public function init(array $config) : void{
+	}
+
+	public function getMoney(Player $player) : void{
+		BedrockEconomyAPI::getInstance()->getPlayerBalance($player->getName());
+		}
+	
+
+	public function addMoney(Player $player, float $money) : void{
+		BedrockEconomyAPI::getInstance()->addToPlayerBalance($player->getName(), (int) ceil($money));
+	}
+
+	//Thanks for the Repair @cooldogedev
+	public function removeMoney(Player $player, float $money) : void{
+		BedrockEconomyAPI::getInstance()->subtractFromPlayerBalance($player->getName(), (int) ceil($money));
+		}
+	
+
+	public function formatMoney(float $money) : string{
+		return $this->plugin->getCurrencyManager()->getSymbol() . number_format($money);
+	}
+}
